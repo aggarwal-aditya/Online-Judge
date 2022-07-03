fefe5a7a7b921f4397ab3bbdfbd2a31215fc7e59
Aditya Aggarwal <aditya.aggarwal393@gmail.com>
2022-07-03 23:54:09 +0530

    Add Homepage & Fix Views

--- onlineJudge/judge/urls.py
+++ onlineJudge/judge/urls.py
@@ -1,9 +1,11 @@
+from tempfile import template
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:problemId>/', views.detail, name='detail'),
+    path('problems', views.problem, name='problem'),
+    path('problems/<int:problemId>/', views.detail, name='detail'),
     # path('<int:problemId>/results/', views.results, name='results'),
 ]
--- onlineJudge/judge/views.py
+++ onlineJudge/judge/views.py
@@ -1,16 +1,23 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from django.http import HttpResponse
+
+# from onlineJudge import judge
 
 
 from .models import Problem
 
 
 def index(request):
+    return render(request, 'judge/index.html')
+
+
+def problem(request):
     latest_question_list = Problem.objects.order_by('difficulty')
     context = {'latest_question_list': latest_question_list}
-    return render(request, 'judge/index.html', context)
+    return render(request, 'judge/problems.html', context)
+
 
 def detail(request, problemId):
     problem = get_object_or_404(Problem, pk=problemId)
